23b1693f9f89c4ca647f9335e82f3e8c97643b71
Gabriel Maciuc <gabrielmaciuc@gmail.com>
2023-08-10 12:44:31 +0300

    Updatat API_server
    Am adaugat 2 obiecte BDQueries, unul care sa apeleze motelede doar pentru tabela "tests", si unul pentru tabela "testruns"

--- API_server.py
+++ API_server.py
@@ -2,42 +2,28 @@ from flask import Flask, jsonify
 from bdqueries import BDQueries
 import yaml
 
-with open("config.yml", "r") as file:
-    config = yaml.safe_load(file)
-
-database_host = config["regression_details"]["database_host"]
-database_port = config["regression_details"]["database_port"]
-database_name = config["regression_details"]["database_name"]
-
-connection_string = "mongodb://" + database_host + ":" + str(database_port)
-
-tests_table_name = "tests"
-testruns_table_name = "testruns"
-
 app = Flask(__name__)
+
 
 @app.route("/api/run_ids", methods=["GET"])
 def get_run_ids():
-    bd_queries = BDQueries(connection_string, database_name, tests_table_name)
-    run_ids = bd_queries.get_run_ids()
+    run_ids = bd_queries_tests.get_run_ids()
     return jsonify({"run_ids": [{"run_name": run_id} for run_id in run_ids]})
 
 
 @app.route("/api/test_names", methods=["GET"])
 def get_test_names():
-    bd_queries = BDQueries(connection_string, database_name, tests_table_name)
-    test_names = bd_queries.get_test_names()
+    test_names = bd_queries_tests.get_test_names()
     return jsonify({"test_names": [{"name": test_name} for test_name in test_names]})
 
 
 @app.route("/api/test_details/<test_name>", methods=["GET"])
 def get_test_details(test_name):
-    bd_queries = BDQueries(connection_string, database_name, tests_table_name)
-    pass_rate = bd_queries.test_pass_rate(test_name)
-    avg_run_time = bd_queries.test_avg_runtime(test_name)
-    max_run_time = bd_queries.test_max_runtime(test_name)
-    min_run_time = bd_queries.test_min_runtime(test_name)
-    avg_sim_time = bd_queries.test_avg_simtime(test_name)
+    pass_rate = bd_queries_tests.test_pass_rate(test_name)
+    avg_run_time = bd_queries_tests.test_avg_runtime(test_name)
+    max_run_time = bd_queries_tests.test_max_runtime(test_name)
+    min_run_time = bd_queries_tests.test_min_runtime(test_name)
+    avg_sim_time = bd_queries_tests.test_avg_simtime(test_name)
 
     if (
         pass_rate is None
@@ -61,9 +47,8 @@ def get_test_details(test_name):
 
 @app.route("/api/execution_details/<run_id>/<test_name>", methods=["GET"])
 def get_execution_details(run_id, test_name):
-    bd_queries = BDQueries(connection_string, database_name, tests_table_name)
-    execution_details = bd_queries.execution_details(run_id, test_name)
-    execution_status = bd_queries.execution_details_status(run_id, test_name)
+    execution_details = bd_queries_tests.execution_details(run_id, test_name)
+    execution_status = bd_queries_tests.execution_details_status(run_id, test_name)
 
     if not execution_details or not execution_status:
         return jsonify({"error": "Execution details not found"}), 404
@@ -81,12 +66,11 @@ def get_execution_details(run_id, test_name):
 
 @app.route("/api/global_summary", methods=["GET"])
 def get_global_summary():
-    bd_queries = BDQueries(connection_string, database_name, testruns_table_name)
-    no_runs = bd_queries.file_no_runs()
-    no_errors = bd_queries.file_no_errors()
-    avg_runtime = bd_queries.file_avg_runtime()
-    avg_sim_time = bd_queries.file_avg_simtime()
-    max_run_time = bd_queries.file_max_runtime()
+    no_runs = bd_queries_testruns.file_no_runs()
+    no_errors = bd_queries_testruns.file_no_errors()
+    avg_runtime = bd_queries_testruns.file_avg_runtime()
+    avg_sim_time = bd_queries_testruns.file_avg_simtime()
+    max_run_time = bd_queries_testruns.file_max_runtime()
 
     if not all([no_runs, no_errors, avg_runtime, avg_sim_time, max_run_time]):
         return jsonify({"error": "Global summary not found"}), 404
@@ -103,4 +87,19 @@ def get_global_summary():
 
 
 if __name__ == "__main__":
+    with open("config.yml", "r") as file:
+        config = yaml.safe_load(file)
+
+    database_host = config["regression_details"]["database_host"]
+    database_port = config["regression_details"]["database_port"]
+    database_name = config["regression_details"]["database_name"]
+
+    connection_string = "mongodb://" + database_host + ":" + str(database_port)
+
+    tests_table_name = "tests"
+    testruns_table_name = "testruns"
+
+    bd_queries_tests = BDQueries(connection_string, database_name, tests_table_name)
+    bd_queries_testruns = BDQueries(connection_string, database_name, testruns_table_name)
+
     app.run(debug=True)
