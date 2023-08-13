23239c605c03a27bbf65998fe30ad7c4af811996
Ionut-Adrian-Olari <100611247+Ionut-Adrian-Olari@users.noreply.github.com>
2023-08-13 11:05:11 +0300

    demo end-to-end

--- bdqueries.py
+++ bdqueries.py
@@ -47,7 +47,7 @@ class BDQueries:
     def test_avg_runtime(self, testname):
         sum_runtime = 0
         count_runtime = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_runtime += document["run_time"]
             count_runtime += 1
@@ -56,7 +56,7 @@ class BDQueries:
     def test_avg_simtime(self, testname):
         sum_simtime = 0
         count_simtime = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_simtime += document["sim_time"]
             count_simtime += 1
@@ -64,7 +64,7 @@ class BDQueries:
 
     def test_max_runtime(self, testname):
         max = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             if max < document["run_time"]:
                 max = document["run_time"]
@@ -72,7 +72,7 @@ class BDQueries:
 
     def test_min_runtime(self, testname):
         min = sys.maxsize
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             if min > document["run_time"]:
                 min = document["run_time"]
@@ -81,7 +81,7 @@ class BDQueries:
     def test_pass_rate(self, testname):
         sum_status = 0
         sum_pass = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_status += 1
             if document["status"].lower() == "pass":
@@ -91,37 +91,37 @@ class BDQueries:
 
     def test_sum_run_time(self, testname):
         sum_runtime = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_runtime += document["run_time"]
         return sum_runtime
 
     def test_last_status(self, testname):
         document = self.collection.aggregate(
-            [{"$sort": {"_id": -1}}, {"$match": {"testname": testname}}, {"$limit": 1}]
+            [{"$sort": {"_id": -1}}, {"$match": {"test_name": testname}}, {"$limit": 1}]
         )
         return list(document)[0]["status"]
 
     # ----------------------------------------------------------------------------------
 
     def execution_details_status(self, filename, testname):
-        documents = self.collection.find({"run_id": filename, "testname": testname})
+        documents = self.collection.find({"run_id": filename, "test_name": testname})
         return list(documents)[0]["status"]
 
     def execution_details(self, filename, testname):
-        documents = self.collection.find({"run_id": filename, "testname": testname})
+        documents = self.collection.find({"run_id": filename, "test_name": testname})
         return list(documents)[0]["loglines"]
     
     def execution_details_run_id(self, testname):
-        documents = self.collection.find({ "testname": testname})
+        documents = self.collection.find({ "test_name": testname})
         return list(documents)[0]["run_id"]
     
     def execution_details_status_run_id(self, testname):
-        documents = self.collection.find({ "testname": testname})
+        documents = self.collection.find({ "test_name": testname})
         return list(documents)[0]["status"]
 
     def execution_details_logline_run_id(self, testname):
-        documents = self.collection.find({ "testname": testname})
+        documents = self.collection.find({ "test_name": testname})
         return list(documents)[0]["loglines"]
 
     # ---------------------------------------------------------------------------------
@@ -138,8 +138,8 @@ class BDQueries:
         documents = self.collection.find({})
         test_names = []
         for document in documents:
-            if document["testname"] not in test_names:
-                test_names.append(document["testname"])
+            if document["test_name"] not in test_names:
+                test_names.append(document["test_name"])
         return test_names
 
 
@@ -152,18 +152,18 @@ def main():
     print("file_max_runtime:", query_testruns.file_max_runtime())
 
     query_tests = BDQueries("mongodb://localhost:27017", "RegressionDetails", "tests")
-    print("test_avg_runtime:", query_tests.test_avg_runtime("tb.test_3"))
-    print("test_avg_simtime:", query_tests.test_avg_simtime("tb.test_3"))
-    print("test_max_runtime:", query_tests.test_max_runtime("tb.test_3"))
-    print("test_min_runtime:", query_tests.test_min_runtime("tb.test_3"))
-    print("test_pass_rate:", query_tests.test_pass_rate("tb.test_3"))
-    print("execution_details_status:",query_tests.execution_details_status("file_3.txt", "tb.test_3"),)
-    print("execution_details:", query_tests.execution_details("file_3.txt", "tb.test_3"))
-    print("execution_details_run_id:", query_tests.execution_details_run_id("tb.test_3"))
+    print("test_avg_runtime:", query_tests.test_avg_runtime("run_test_004"))
+    print("test_avg_simtime:", query_tests.test_avg_simtime("run_test_004"))
+    print("test_max_runtime:", query_tests.test_max_runtime("run_test_004"))
+    print("test_min_runtime:", query_tests.test_min_runtime("run_test_004"))
+    print("test_pass_rate:", query_tests.test_pass_rate("run_test_004"))
+    print("execution_details_status:",query_tests.execution_details_status("1", "run_test_004"),)
+    print("execution_details:", query_tests.execution_details("1", "run_test_004"))
+    print("execution_details_run_id:", query_tests.execution_details_run_id("run_test_004"))
     print("get_run_ids:", query_tests.get_run_ids())
     print("get_test_names:", query_tests.get_test_names())
-    print("test_sum_run_time:", query_tests.test_sum_run_time("tb.test_3"))
-    print("test_last_status:", query_tests.test_last_status("tb.test_2"))
+    print("test_sum_run_time:", query_tests.test_sum_run_time("run_test_004"))
+    print("test_last_status:", query_tests.test_last_status("run_test_005"))
 
 
 if __name__ == "__main__":
