da48bd2da762ee7c5dec0e87e08481f4a0b89fcf
Gabriel Maciuc <gabrielmaciuc@gmail.com>
2023-08-13 23:44:45 +0300

    Update bdqueries.py

--- bdqueries.py
+++ bdqueries.py
@@ -47,7 +47,7 @@ class BDQueries:
     def test_avg_runtime(self, testname):
         sum_runtime = 0
         count_runtime = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_runtime += document["run_time"]
             count_runtime += 1
@@ -56,7 +56,7 @@ class BDQueries:
     def test_avg_simtime(self, testname):
         sum_simtime = 0
         count_simtime = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_simtime += document["sim_time"]
             count_simtime += 1
@@ -64,7 +64,7 @@ class BDQueries:
 
     def test_max_runtime(self, testname):
         max = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             if max < document["run_time"]:
                 max = document["run_time"]
@@ -72,7 +72,7 @@ class BDQueries:
 
     def test_min_runtime(self, testname):
         min = sys.maxsize
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             if min > document["run_time"]:
                 min = document["run_time"]
@@ -81,7 +81,7 @@ class BDQueries:
     def test_pass_rate(self, testname):
         sum_status = 0
         sum_pass = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_status += 1
             if document["status"].lower() == "pass":
@@ -91,25 +91,25 @@ class BDQueries:
 
     def test_sum_run_time(self, testname):
         sum_runtime = 0
-        documents = self.collection.find({"testname": testname})
+        documents = self.collection.find({"test_name": testname})
         for document in documents:
             sum_runtime += document["run_time"]
         return sum_runtime
 
     def test_last_status(self, testname):
         document = self.collection.aggregate(
-            [{"$sort": {"_id": -1}}, {"$match": {"testname": testname}}, {"$limit": 1}]
+            [{"$sort": {"_id": -1}}, {"$match": {"test_name": testname}}, {"$limit": 1}]
         )
         return list(document)[0]["status"]
 
     # ----------------------------------------------------------------------------------
 
     def execution_details_status(self, filename, testname):
-        documents = self.collection.find({"run_id": filename, "testname": testname})
+        documents = self.collection.find({"run_id": filename, "test_name": testname})
         return list(documents)[0]["status"]
 
     def execution_details(self, filename, testname):
-        documents = self.collection.find({"run_id": filename, "testname": testname})
+        documents = self.collection.find({"run_id": filename, "test_name": testname})
         return list(documents)[0]["loglines"]
 
     # ---------------------------------------------------------------------------------
@@ -126,8 +126,8 @@ class BDQueries:
         documents = self.collection.find({})
         test_names = []
         for document in documents:
-            if document["testname"] not in test_names:
-                test_names.append(document["testname"])
+            if document["test_name"] not in test_names:
+                test_names.append(document["test_name"])
         return test_names
 
 
