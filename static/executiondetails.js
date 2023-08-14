7e52ac720148cd63436dd3e8f65c046bd56ea5ae
Ionut-Adrian-Olari <100611247+Ionut-Adrian-Olari@users.noreply.github.com>
2023-08-14 12:44:56 +0300

    static file update

--- static/executiondetails.js
+++ static/executiondetails.js
@@ -1,121 +1,104 @@
-// fetch('/api/run_ids')
-//    .then(response => response.json())
-//    .then(data => {
-//        const runIdSelect = document.getElementById('comboBoxRunId');
+
+
+
+
+fetch('/api/run_ids')
+   .then(response => response.json())
+   .then(data => {
+       const runIdSelect = document.getElementById('comboBoxRunId');
 
 
        
-//        data.run_ids.forEach(run => {
-//            const option = document.createElement('option');
-//            option.value = run.run_name;
-//            option.textContent = run.run_name;
-//            runIdSelect.appendChild(option);
+       data.run_ids.forEach(run => {
+           const option = document.createElement('option');
+           option.value = run.run_name;
+           option.textContent = run.run_name;
+           runIdSelect.appendChild(option);
 
            
-//        });
+       });
        
       
-//    })
-//    .catch(error => {
-//        console.error('Error fetching or parsing JSON:', error);
-//    });
+   })
+   .catch(error => {
+       console.error('Error fetching or parsing JSON:', error);
+   });
 
-//    fetch('/api/test_names')
-//            .then(response => response.json())
-//            .then(data => {
-//            const testNameSelect = document.getElementById('comboBoxTestNames');
+   fetch('/api/test_names')
+           .then(response => response.json())
+           .then(data => {
+           const testNameSelect = document.getElementById('comboBoxTestNames');
             
-//            data.test_names.forEach(test => {
-//              const option = document.createElement('option');
-//              option.value = test.name;
-//              option.textContent = test.name;
-//              testNameSelect.appendChild(option);
-//        });
-//      })
-//       .catch(error => {
-//        console.error('Error fetching or parsing JSON:', error);
-//       });
+           data.test_names.forEach(test => {
+             const option = document.createElement('option');
+             option.value = test.name;
+             option.textContent = test.name;
+             testNameSelect.appendChild(option);
+       });
+     })
+      .catch(error => {
+       console.error('Error fetching or parsing JSON:', error);
+      });
 
 
-  
-
-// const runIdSelect = document.getElementById('comboBoxRunId');
-// const testNameSelect = document.getElementById('comboBoxTestNames');
-//  const testNameSelectAlone = document.getElementById('comboBoxTestNames');
-// const executionDetailsDiv = document.getElementById('log_lineItem');
-// const inputPassOrFail = document.getElementById('statusInput');
-
-// // testNameSelect.addEventListener('change', () => {
-// //     const selectedTestName = testNameSelect.value;
-
-// //      fetch(`/api/execution_details/${selectedTestName}`)
-// //       .then(response => response.json())
-// //        .then(data => {
-// //       if (data.error) {
-// //        executionDetailsDiv.textContent = 'Execution details not found';
-// //        } else {
-// //          const executionDetails = data.execution_details;
-// //          inputPassOrFail.value = executionDetails.status;
-// //           runIdSelect.value = executionDetails.run_id;
-// //          executionDetailsDiv.innerHTML = `
-// //           <p> ${executionDetails.logline}</p>
-
-// //       `;
-// //   }
-// // })
-// // .catch(error => console.error('Error fetching execution details:', error));
-// // });
-
-
-// testNameSelect.addEventListener('change', () => {
-// const selectedRunId = runIdSelect.value;
-// const selectedTestName = testNameSelect.value;
-// if(selectedRunId == null){
-//  console.log('Run id not selected!')}
-// else{
-//  fetch(`/api/execution_details/${selectedRunId}/${selectedTestName}`)
-//    .then(response => response.json())
-//    .then(data => {
-//        if (data.error) {
-//            executionDetailsDiv.textContent = 'Execution details not found';
-//        } else {
-//            const executionDetails = data.execution_details;
-//            inputPassOrFail.value = executionDetails.status;
-
-//            executionDetailsDiv.innerHTML = `
-//                <p> ${executionDetails.logline}</p>
-
-//            `;
-//        }
-//    })
-//    .catch(error => console.error('Error fetching execution details:', error));
-// }
-
-
-// });
-
-// testNameSelectAlone.addEventListener('change', () => {
-// const selectedTestNameAlone = testNameSelectAlone.value;
-
-//  fetch(`/api/execution_details/${selectedTestNameAlone}`)
-//    .then(response => response.json())
-//    .then(data => {
-//        if (data.error) {
-//            executionDetailsDiv.textContent = 'Execution details not found';
-//        } else {
-//            const executionDetails = data.execution_details;
-//            inputPassOrFail.value = executionDetails.status;
-//            runIdSelect.value = executionDetails.run_id;
-//            executionDetailsDiv.innerHTML = `
-//                <p> ${executionDetails.logline}</p>
-
-//            `;
-//        }
-//    })
-//    .catch(error => console.error('Error fetching execution details:', error));
-
-
-
-// });
+      document.addEventListener('DOMContentLoaded', () => {
+        const runIdSelect = document.getElementById('comboBoxRunId');
+        const testNameSelect = document.getElementById('comboBoxTestNames');
+        const testNameSelectAlone = document.getElementById('comboBoxTestNames');
+        const executionDetailsDiv = document.getElementById('log_lineItem');
+        const inputPassOrFail = document.getElementById('statusInput');
+      
+testNameSelect.addEventListener('change', () => {
+    const selectedRunId = runIdSelect.value;
+    const selectedTestName = testNameSelect.value;
+    if(selectedRunId == null){
+     console.log('Run id not selected!')}
+    else{
+     fetch(`/api/execution_details/${selectedRunId}/${selectedTestName}`)
+       .then(response => response.json())
+       .then(data => {
+           if (data.error) {
+               executionDetailsDiv.textContent = 'Execution details not found';
+           } else {
+               const executionDetails = data.execution_details;
+               inputPassOrFail.value = executionDetails.status;
+    
+               executionDetailsDiv.innerHTML = `
+                   <p> ${executionDetails.logline}</p>
+    
+               `;
+           }
+       })
+       .catch(error => console.error('Error fetching execution details:', error));
+    }
+    
+    
+    });
+    
+    testNameSelectAlone.addEventListener('change', () => {
+    const selectedTestNameAlone = testNameSelectAlone.value;
+    
+     fetch(`/api/execution_details/${selectedTestNameAlone}`)
+       .then(response => response.json())
+       .then(data => {
+           if (data.error) {
+               executionDetailsDiv.textContent = 'Execution details not found';
+           } else {
+               const executionDetails = data.execution_details;
+               inputPassOrFail.value = executionDetails.status;
+               runIdSelect.value = executionDetails.run_id;
+               executionDetailsDiv.innerHTML = `
+                   <p> ${executionDetails.logline}</p>
+    
+               `;
+           }
+       })
+       .catch(error => console.error('Error fetching execution details:', error));
+    
+    
+    
+    });
+    
+      }); 
 
 
--- static/index.js
+++ static/index.js
@@ -1,42 +1,48 @@
 
 
-const summaryContainer = document.getElementById("tableGlobalSummary");
+
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const summaryContainer = document.getElementById('tableGlobalSummary');
  
-   fetch("/api/global_summary")
+fetch("/api/global_summary")
   .then((response) => response.json())
   .then(data => { const globalSummary = data.global_summary;
 
-  
-  summaryContainer.innerHTML = `
-  <tr >
-              <th scope="col"  colspan="3">Global Summary</th>
-            </tr>
-  <tr>
-    <th scope="row">No. Run</th>
-          <td>${globalSummary.no_runs}</td>
-          <td>/</td>
-          
-    </tr>
-    <tr>
-              <th scope="col">No. Errors</th>
-              <td>${globalSummary.no_errors}</td>
-              <td>s</td>
-              
-    </tr>
-    <tr>
-              <th scope="row">Avg Runtime</th>
-              <td>${globalSummary.avg_runtime}</td>
-              <td>s</td>
-            </tr>
-    <tr>
-              <th scope="row">Avg Sim Time</th>
-              <td>${globalSummary.avg_sim_time.toFixed(2)}</td>
-              <td>s</td>
-    </tr>
-    <tr>
-              <th scope="row">Max Run Time</th>
-              <td>${globalSummary.max_run_time}</td>
-              <td>s</td>
-    </tr>
-`;
+           let indexHtml = '';
+           indexHtml =`
+            <tr>
+             <th scope="col"  colspan="3">Global Summary</th>
+           </tr>
+           <tr>
+            <th scope="row">No. Run</th>
+             <td>${globalSummary.no_runs}</td>
+           <td>/</td>  
+           </tr>
+           <tr>
+           <th scope="col">No. Errors</th>
+             <td>${globalSummary.no_errors}</td>
+             <td>s</td>
+           </tr>
+           <tr>
+             <th scope="row">Avg Runtime</th>
+             <td>${globalSummary.avg_runtime}</td>
+             <td>s</td>
+           </tr>
+           <tr>
+             <th scope="row">Avg Sim Time</th>
+             <td>${globalSummary.avg_sim_time.toFixed(2)}</td>
+             <td>s</td>
+           </tr>
+           <tr>
+             <th scope="row">Max Run Time</th>
+             <td>${globalSummary.max_run_time}</td>
+             <td>s</td>
+           </tr>`;
+
+    summaryContainer.innerHTML = indexHtml;
+  })
+      .catch(error => {
+        console.error('An error occurred:', error);
+      });
   });
